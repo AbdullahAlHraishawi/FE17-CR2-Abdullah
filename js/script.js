c7001825b9dd7008463918fda4ea8078a8ce8839
AbdullahAlHraishawi <abdullah.alhraishawi@gmail.com>
2022-10-01 16:17:40 +0200

    Second CodeReview

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
 let tasksList = JSON.parse(tasks);
 
-for (let tasksListItem of tasksList){
-    document.getElementById("tasks").innerHTML += `
-
-    <div class="col">
-        <div id="shadow" class="card">
-            <img src="${tasksListItem.image}" class="card-img-top" alt="walk the dog" />
-            <div class="card-body">
-              <h5 class="card-title">${tasksListItem.taskName}</h5>
-              <p class="card-text">${tasksListItem.description}</p>
-              <hr>
-              <h6><img id="priorityIcon" src="images/alert-triangle.svg" alt="alert triangle"> Priority level: <span id="priority">${tasksListItem.importance}</span></h6>
-              <h6><img id="priorityIcon" src="images/calendar.svg" alt="calender"> Deadline: ${tasksListItem.deadline}</h6>
-              <hr>
-              <button id="button" type="button" class="btn btn-success"><img id="priorityIcon" src="images/check-circle.svg" alt="check"> Done</button>
-              <button id="button" type="button" class="btn btn-danger"><img id="priorityIcon" src="images/trash.svg" alt="trash can"> Delete</button>
-              
+function updateList() {
+    for (let tasksListItem of tasksList) {
+        document.getElementById("tasks").innerHTML += `
+        
+            <div class="col">
+                <div id="shadow" class="card">
+                    <img src="${tasksListItem.image}" class="card-img-top" alt="walk the dog" />
+                    <div class="card-body">
+                        <h5 class="card-title">${tasksListItem.taskName}</h5>
+                        <p class="card-text">${tasksListItem.description}</p>
+                        <hr>
+                        <h6><img id="icons" src="images/alert-triangle.svg" alt="alert triangle"> Priority level: <button class="priorityBtn" type="button">${tasksListItem.importance}</button></h6>
+                        <h6><img id="icons" src="images/calendar.svg" alt="calender"> Deadline: ${tasksListItem.deadline}</h6>
+                        <hr>
+                        <button id="button" type="button" class="btn btn-success"><img id="priorityIcon" src="images/check-circle.svg" alt="check"> Done</button>
+                        <button id="button" type="button" class="btn btn-danger"><img id="priorityIcon" src="images/trash.svg" alt="trash can"> Delete</button>
+                        
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-
-    `;
-
-    
+        
+        `;
+    }
 }
+
+updateList();
+addAction();
+
+function priorityFunction(index) {
+    if (tasksList[index].importance != 5) {
+        tasksList[index].importance++;
+        document.getElementsByClassName("priorityBtn")[index].innerHTML =
+        tasksList[index].importance;
+    }
+}
+
+function sortByPriority() {
+    tasksList.sort((a, b) => b.importance - a.importance);
+    document.getElementById("tasks").innerHTML = "";
+    updateList();
+    addAction();
+}
+
+function addAction() {
+    let priorityBtns = document.getElementsByClassName("priorityBtn");
+
+    for (let i = 0; i < priorityBtns.length; i++) {
+        priorityBtns[i].addEventListener("click", function () {
+        priorityFunction(i);
+        });
+    }
+}
+
+document.getElementById("sortImg").onclick = sortByPriority;
